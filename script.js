da94ce58358355d8dabe8a8cdce2fa47b90c45e6
Diego <diegogode.bonani@gmail.com>
2023-06-17 21:49:51 -0300

    feito

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ const pokemons = [
   { nome: 'Squirtle', tipo: 'água', vida: 45 },
   { nome: 'Psyduck', tipo: 'água', vida: 25 }
 ]
+function mudaVida(poke){
+    const novoPoke = {
+        nome: poke.nome,
+        tipo: poke.tipo,
+        vida: 100,
+    }
+    return novoPoke;
+}
+function checaTipo(poke){
+    let tipo = poke.tipo === "grama";
+    return tipo; 
+}
+const novoArray = pokemons.map(mudaVida);
+const outroArray = pokemons.filter(checaTipo);
+console.log(novoArray);
+console.log(outroArray)
